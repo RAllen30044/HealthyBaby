3738a357595a525930226754517246f6a8652c98
Robert <robharmony@hotmail.com>
2023-12-03 18:53:11 -0500

    added ActiveC

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { ActiveComponentProvider } from "./assets/Header/ActiveComponent.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Toaster />
-    <App />
+    <ActiveComponentProvider>
+      <Toaster />
+      <App />
+    </ActiveComponentProvider>
   </React.StrictMode>
 );
